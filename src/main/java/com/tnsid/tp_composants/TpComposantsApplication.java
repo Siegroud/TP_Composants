e364080df9e28ea6215b79574ce4e853ea600920
BOSQUET Siegrid <siegrid@outlook.fr>
2023-12-28 14:32:59 +0100

    Initial commit

---
+++ src/main/java/com/tnsid/tp_composants/TpComposantsApplication.java
@@ -0,0 +1,13 @@
+package com.tnsid.tp_composants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpComposantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpComposantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tnsid/tp_composants/TpComposantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsid.tp_composants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpComposantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
